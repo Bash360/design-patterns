62a2fb3f93b0c88418d6d4e9a4cb5fa65889db79
mark bashir <beejayphil@gmail.com>
2020-02-01 11:44:27 +0100

    documented some shit

--- src/main/java/creationalpatterns/singleton/DbSingleton.java
+++ src/main/java/creationalpatterns/singleton/DbSingleton.java
@@ -1,5 +1,12 @@
 package creationalpatterns.singleton;
 
+/* manage object life cycle
+private constructors to avoid user creation of classes
+and the variable should be static to have one of it for every class
+and a method to get instance
+no argument should be passed
+*/
+
 public class DbSingleton {
   private DbSingleton() {}
 
