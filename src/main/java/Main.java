9afe99f29be451302a9672a34cbc2cc75cf4e510
mark bashir <beejayphil@gmail.com>
2020-02-01 19:10:25 +0100

    throw error to avoid class instance from being created with java reflection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 import creationalpatterns.singleton.LazyLoadedSingleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args)
+      throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException,
+          InstantiationException, InvocationTargetException {
     LazyLoadedSingleton db = LazyLoadedSingleton.getInstance();
     LazyLoadedSingleton second = LazyLoadedSingleton.getInstance();
+    Class random = Class.forName("creationalpatterns.singleton.LazyLoadedSingleton");
+    Constructor bash = random.getDeclaredConstructor();
+    bash.setAccessible(true);
+    LazyLoadedSingleton loaded = (LazyLoadedSingleton) bash.newInstance(null);
+    System.out.println(loaded);
+    Method m = random.getDeclaredMethod("getInstance", null);
+    System.out.println(m.invoke(bash, null));
     System.out.println(db);
     System.out.println(second);
   }
--- src/main/java/creationalpatterns/singleton/LazyLoadedSingleton.java
+++ src/main/java/creationalpatterns/singleton/LazyLoadedSingleton.java
@@ -3,7 +3,11 @@ package creationalpatterns.singleton;
 public class LazyLoadedSingleton {
   private static volatile LazyLoadedSingleton instance = null;
 
-  private LazyLoadedSingleton() {}
+  private LazyLoadedSingleton() {
+    if (instance != null) {
+      throw new RuntimeException("use get instance to create object");
+    }
+  }
 
   public static LazyLoadedSingleton getInstance() {
     if (instance == null) {
