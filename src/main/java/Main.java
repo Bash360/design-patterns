acb0317b30d157bbb192602d50fa8b39795fa485
mark bashir <beejayphil@gmail.com>
2020-02-02 01:36:38 +0100

    added the builder pattern

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,18 @@
-import creationalpatterns.singleton.LazyLoadedSingleton;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import creationalpatterns.builder.LunchOrder;
 
 public class Main {
-  public static void main(String[] args)
-      throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException,
-          InstantiationException, InvocationTargetException {
-    LazyLoadedSingleton db = LazyLoadedSingleton.getInstance();
-    LazyLoadedSingleton second = LazyLoadedSingleton.getInstance();
-    Class random = Class.forName("creationalpatterns.singleton.LazyLoadedSingleton");
-    Constructor bash = random.getDeclaredConstructor();
-    bash.setAccessible(true);
-    LazyLoadedSingleton loaded = (LazyLoadedSingleton) bash.newInstance(null);
-    System.out.println(loaded);
-    Method m = random.getDeclaredMethod("getInstance", null);
-    System.out.println(m.invoke(bash, null));
-    System.out.println(db);
-    System.out.println(second);
+  public static void main(String[] args) {
+    LunchOrder.Builder builder = new LunchOrder.Builder();
+    LunchOrder myLunch =
+        builder
+            .bread("sliced bread")
+            .condiments("yes butter and sardine")
+            .dressing("foil wrapper")
+            .meat("steak")
+            .build();
+    System.out.println(myLunch.getBread());
+    System.out.println(myLunch.getCondiments());
+    System.out.println(myLunch.getDressing());
+    System.out.println(myLunch.getMeat());
   }
 }
---
+++ src/main/java/creationalpatterns/builder/LunchOrder.java
@@ -0,0 +1,64 @@
+package creationalpatterns.builder;
+
+public class LunchOrder {
+  private String bread = null;
+  private String condiments = null;
+  private String meat = null;
+  private String dressing = null;
+
+  public static class Builder {
+    private String bread;
+    private String condiments;
+    private String meat;
+    private String dressing;
+
+    public Builder() {}
+
+    public Builder bread(String bread) {
+      this.bread = bread;
+      return this;
+    }
+
+    public Builder condiments(String condiments) {
+      this.condiments = condiments;
+      return this;
+    }
+
+    public Builder meat(String meat) {
+      this.meat = meat;
+      return this;
+    }
+
+    public Builder dressing(String dressing) {
+      this.dressing = dressing;
+      return this;
+    }
+
+    public LunchOrder build() {
+      return new LunchOrder(this);
+    }
+  }
+
+  private LunchOrder(Builder builder) {
+    this.bread = builder.bread;
+    this.condiments = builder.condiments;
+    this.dressing = builder.dressing;
+    this.meat = builder.meat;
+  }
+
+  public String getBread() {
+    return bread;
+  }
+
+  public String getCondiments() {
+    return condiments;
+  }
+
+  public String getMeat() {
+    return meat;
+  }
+
+  public String getDressing() {
+    return dressing;
+  }
+}
