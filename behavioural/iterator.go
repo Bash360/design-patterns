f258903c71a17177d4e3da9ceb599bbba61eda27
bash360 <mark.bashir.ugwi@gmail.com>
2025-04-29 21:03:40 +0100

    iterator

--- behavioural/iterator.go
+++ behavioural/iterator.go
@@ -1,31 +1,68 @@
-package behavioural  
+package behavioural
 
-type Iterator interface{
+type Iterator interface {
 	HasNext() bool
 	Next() any
 }
 
-type SeriesIterator struct{
-index int
-arr []any
+type ArrayIterator struct {
+	index int
+	arr   []any
 }
 
-func(s *SeriesIterator) HasNext() bool {
+func (s *ArrayIterator) HasNext() bool {
 	return s.index < len(s.arr)
 }
 
-func(s *SeriesIterator)Next() any{
-	if !s.HasNext(){
+func (s *ArrayIterator) Next() any {
+	if !s.HasNext() {
 		return nil
 	}
-	item:= s.arr[s.index]
+	item := s.arr[s.index]
 
 	s.index++
 	return item
 }
 
-func NewSeries(array []any) *SeriesIterator{
-	return & SeriesIterator{arr: array}
+type MapIterator struct {
+	keys  []any
+	index int
+	maps  map[any]any
+}
+
+func (m *MapIterator) HasNext() bool {
+	return m.index < len(m.keys)
+}
+
+func (m *MapIterator) Next() any {
+
+	if !m.HasNext() {
+		return nil
+	}
+
+	key := m.keys[m.index]
+	value := m.maps[key]
+	m.index += 1
+	return value
+
+}
+func NewIterator(collection any) Iterator {
+	
+	switch c:=collection.(type) {
+	case []any:
+		return &ArrayIterator{arr: c}
+	case map[any]any:
+
+		mapKeys := make([]any, 0)
+		for key := range c{
+			mapKeys = append(mapKeys, key)
+		}
+
+		return &MapIterator{keys: mapKeys, maps: c}
+	default:
+		return nil
+
+	}
 }
 
 /*
--- main.go
+++ main.go
@@ -1,23 +1,6 @@
 package main
-
-import (
-	"design-patterns/behavioural"
-	"fmt"
-)
 func main(){
 
-	season:=[]any{
-		map[string]any{"title": "Fall From Grace - Ep 1", "duration": "45m", "released": true},
-		map[string]any{"title": "Fall From Grace - Ep 2", "duration": "50m", "released": true},
-		map[string]any{"title": "Fall From Grace - Ep 3", "duration": "48m", "released": false},
-	}
-	
-  series := behavioural.NewSeries(season)
-
- fmt.Println(series.Next())
- fmt.Println(series.Next())
- fmt.Println(series.Next())
- fmt.Println(series.Next())
 	
 }
 
