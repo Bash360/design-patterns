383d6b55cbbc330b3c19471e0b1edf438ca5e295
bash360 <mark.bashir.ugwi@gmail.com>
2025-04-29 21:31:59 +0100

    observer pattern

--- behavioural/observer.go
+++ behavioural/observer.go
@@ -1,1 +1,54 @@
 package behavioural
+
+import "fmt"
+
+type Observer interface{
+	Update(data any)
+}
+
+type Subject interface{
+	Register(observer Observer)
+	Unregister(observer Observer)
+	Notify(data any)
+}
+
+
+type TutorialChannel struct{
+	observers []Observer
+}
+
+func (t *TutorialChannel) Register(observer Observer){
+	t.observers = append(t.observers, observer)
+}
+
+func (t *TutorialChannel) Unregister(observer Observer){
+	for i, obs := range t.observers{
+		if obs == observer{
+			t.observers = append(t.observers[:i], t.observers[i+1:]...)
+			break
+		}
+	}
+}
+
+func (t *TutorialChannel) Notify(data any){
+	for _, obs := range t.observers{
+		obs.Update(data)
+	}
+}
+
+type EmailSubscriber struct{
+	Email string
+}
+
+func (e *EmailSubscriber) Update(data any){
+fmt.Println("Email to ",e.Email,":",data)
+
+}		
+
+
+
+/*
+useful when we have a list of dependents on an object and they need to be notified automatically when theres a state change in the object kind of like in a pub/sub system
+
+
+*/
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
+
+import "design-patterns/behavioural"
 func main(){
 
-	
+	channel:= &behavioural.TutorialChannel{}
+
+	channel.Register(&behavioural.EmailSubscriber{Email: "beejayphil@gmail.com"})
+	channel.Register(&behavioural.EmailSubscriber{Email: "kenzy@gmail.com"})
+	channel.Notify(map[string]string{
+		"tutorial": "learning goroutines",
+	})
 }
 
